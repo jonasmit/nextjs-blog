1418e97f26d7fee97d526f1598eacc57ec45b151
jonasmit <jon.a.smith@gmail.com>
2022-01-31 21:39:04 -0500

    setting up dynamic routing

--- content/lessons/kubernetes-for-beginners.md
+++ content/lessons/kubernetes-for-beginners.md
@@ -1,5 +1,5 @@
 ---
-title: "Two Forms of Pre-rendering"
+title: "Kubernetes For Beginners"
 date: "2020-01-01"
 course: "kubernetes-for-beginners"
 ---
--- pages/course/[id].js
+++ pages/course/[id].js
@@ -1,48 +1,13 @@
 import Layout from "../../components/layout";
-import { getAllLessonIds, getSortedLessonsData } from "../../lib/posts";
-import Date from "../../components/date";
-import Head from "next/head";
-import utilStyles from "../../styles/utils.module.css";
+import { useRouter } from "next/router";
 
-export async function getStaticProps() {
-  const allLessonsData = getSortedLessonsData();
-  return {
-    props: {
-      allLessonsData,
-    },
-  };
-}
+export default function Post() {
+  const router = useRouter()
+  const { id } = router.query
 
-export async function getStaticPaths() {
-  const paths = getAllLessonIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export default function Post({ allLessonsData }) {
-  return (
+  return ( 
     <Layout>
-      <Head>
-        <title>{allLessonsData.title}</title>
-      </Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allLessonsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+      <p>Post: {id}</p>
+    </Layout> 
   );
 }
