4a2d8fd2f0b92f6c9341220bbf7927c4ae49d906
Jon Smith <jon.a.smith@gmail.com>
2022-02-03 08:25:14 -0500

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import Date from "../components/date";
-import content from "../content/featured.courses.json";
 import Duration from "../components/duration";
+import { getSortedCourseData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allCourseData }) {
   return (
     <Layout home>
       <Head>
@@ -19,25 +19,39 @@ export default function Home() {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Featured Courses</h2>
         <ul className={utilStyles.list}>
-          {content.courses.map(({ id, date, duration, lessons, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <small className={utilStyles.lightText}>
-                Duration: <Duration durationInMinutes={duration} />
-              </small>
-              <br />
-              <Link href={`/course/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-              <br />
-              <small className={utilStyles.lightText}>Lessons: {lessons}</small>
-            </li>
-          ))}
+          {allCourseData.map(
+            ({ id, date, title, description, duration, lessons }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <small className={utilStyles.lightText}>
+                  Duration: <Duration durationInMinutes={duration} />
+                </small>
+                <br />
+                <Link href={`/course/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+                <br />
+                <small className={utilStyles.lightText}>
+                  Lessons: {lessons}
+                </small>
+              </li>
+            )
+          )}
         </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allCourseData = getSortedCourseData();
+  console.log(allCourseData);
+  return {
+    props: {
+      allCourseData,
+    },
+  };
 }
